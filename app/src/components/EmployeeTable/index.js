6884730ef6191745eeb7f80a663a7e66f1f294c8
Brian Lee <brianlee@Brians-MacBook-Pro.local>
2020-11-14 22:17:09 -0800

    still bugs in employeetable

--- app/src/components/EmployeeTable/index.js
+++ app/src/components/EmployeeTable/index.js
@@ -2,48 +2,57 @@ import React, { Component } from "react";
 import EmployeeTableTemplate from "../EmployeeTableTemplate"
 import Api from "../../utils/Api"
 
-class EmployeeTable extends Component{
+class EmployeeTable extends Component {
     state = {
         employees: [{}],
         search: "",
         filteredEmployees: [{}],
-        
+        sorted: false,
+        loaded: false,
     }
-//use componentDidMount to require Dom Nodes
-componentDidMount() {
-    Api.returnEmployees().then(res => this.setState({ employees: res.data.results}));
-}
+    //use componentDidMount to require Dom Nodes
+    componentDidMount() {
+        Api.returnEmployees().then(res => this.setState({ employees: res.data.results }));
+    }
 
-handleSubmit(event) {
-    event.preventDefault();
+    handleSubmit(event) {
+        event.preventDefault();
+        let { search } = this.state.search;
+        console.log(search);
+        const lowerCaseSearch = search.toLowerCase();
+        console.log(lowerCaseSearch);
+        let { employees } = this.state.employees;
+        console.log(employees);
+        let filteredEmployees = employees.filter(employee => {
+            if (employee.name.first.toLowerCase.includes(lowerCaseSearch) || employee.name.last.toLowerCase().includes(lowerCaseSearch)) {
+                return employee;
+            }
+        });
+        console.log(filteredEmployees);
+        this.setState({ filteredEmployees: filteredEmployees });
+        this.setState({ search: search });
+        this.setState({ sorted: true });
+    }
 
-    let { search } = this.state.search;
-    const lowerCaseSearch = search.toLowerCase();
-    let { employees } = this.state.employees;
-    let filteredEmployees = employees.filter(employee => {
-        if (employee.name.first.toLowerCase.includes(lowerCaseSearch)||employee.name.last.toLowerCase().includes(lowerCaseSearch)){
-        return employee;
+    returnEmployees() {
+        if (this.state.sorted) {
+            return (
+                <div>
+                    {this.state.filtered.map(employee => (
+                        <EmployeeTableTemplate
+                            picture={employee.picture.medium}
+                            name={employee.name.first + " " + employee.name.last}
+                            phone={employee.phone}
+                            email={employee.email}
+                            dob={employee.dob.date}
+                        />
+                    ))}
+                </div>
+            );
         }
-    });
-    this.setState({ filteredEmployees: filteredEmployees });
-    this.setState({ search : search});
-}
-
-returnEmployees() {
-    return(
-        <div>
-            {this.state.filtered.map(employee => (
-                 <EmployeeTableTemplate
-                    picture = {employee.picture.medium}
-                    name = {employee.name.first+ " " + employee.name.last}
-                    phone = {employee.phone}
-                    email = {employee.email}
-                    dob = {employee.dob.date}
-                />
-            ))}
-        </div>
-    )
-}
+        else
+            return;
+    }
 
 }
 export default EmployeeTable;
